65407c27b859062d978a2f0a0911ee876d64555c
Petter <petter.fogel@medieinstitutet.se>
2021-01-25 17:14:00 +0100

    Add some basic logic for fetching user input values.

--- dist/login.js
+++ dist/login.js
@@ -1,11 +1,19 @@
 "use strict";
+window.addEventListener("load", testP);
+function testP() {
+    loginTest();
+}
 function loginTest() {
     const loginForm = document.getElementById("login-form");
-    loginForm.addEventListener("submit", () => {
-        test();
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        checkValidUserInputValues();
     });
 }
-function test() {
-    console.log("test");
+function checkValidUserInputValues() {
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 }
 //# sourceMappingURL=login.js.map
--- src/login.ts
+++ src/login.ts
@@ -1,10 +1,20 @@
+window.addEventListener("load", testP);
+
+function testP() {
+    loginTest();
+}
+
 function loginTest() {
     const loginForm = document.getElementById("login-form") as HTMLFormElement;
-    loginForm.addEventListener("submit", () => {
-        test();
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        checkValidUserInputValues();
     });
 }
 
-function test() {
-    console.log("test");    
+function checkValidUserInputValues() {
+    const usernameInput = document.getElementById("username") as HTMLInputElement;
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 }
